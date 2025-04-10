d5f33bcc6c34fa83261814aa73f08ee452e10602
Aran Fononi <aaronfononi@gmail.com>
2025-04-10 13:59:43 +0330

    Initial Commit

---
+++ Online Shop App - SwiftUI Masterclass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Online Shop App - SwiftUI Masterclass
+//
+//  Created by Aran Fononi on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
