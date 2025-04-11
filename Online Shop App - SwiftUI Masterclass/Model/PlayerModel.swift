2e34e17ead3749c3c65bbfbdc4a5732dff07deaf
Aran Fononi <aaronfononi@gmail.com>
2025-04-11 19:05:59 +0330

    Create Player Data model

---
+++ Online Shop App - SwiftUI Masterclass/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Online Shop App - SwiftUI Masterclass
+//
+//  Created by Aran Fononi on 11/4/25.
+//
+
+import Foundation
+
+struct Player : Codable, Identifiable {
+    let id: Int
+    let image: String
+}
