39dde098728ca331b7e6dcc5132b5b64aba90ccc
Aran Fononi <aaronfononi@gmail.com>
2025-04-14 13:25:46 +0330

    Add ProductItems to Content View using LazyVGrid

--- Online Shop App - SwiftUI Masterclass/App/ContentView.swift
+++ Online Shop App - SwiftUI Masterclass/App/ContentView.swift
@@ -31,6 +31,13 @@ struct ContentView: View {
                         
                         TitleView(title: "Helmets")
                         
+                        LazyVGrid(columns: gridLayout,spacing: 15) {
+                            ForEach(products) { product in
+                            ProductItemView(product: product)
+                            } //: Loop
+                        } //: Grid
+                        .padding(15)
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VStack
