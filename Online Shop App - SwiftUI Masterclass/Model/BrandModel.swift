a395c384a25f8f2895be282e99cbc0ce5b070c2a
Aran Fononi <aaronfononi@gmail.com>
2025-04-15 09:57:48 +0330

    create Brand data model for JSON file and add constant for it

---
+++ Online Shop App - SwiftUI Masterclass/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Online Shop App - SwiftUI Masterclass
+//
+//  Created by Aran Fononi on 15/4/25.
+//
+
+import Foundation
+
+struct Brand : Codable, Identifiable {
+    let id: Int
+    let image: String
+}
