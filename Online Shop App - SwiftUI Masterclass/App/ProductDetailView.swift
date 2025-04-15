50262ca4f27f6849110868ad5975bf40f77a8dcf
Aran Fononi <aaronfononi@gmail.com>
2025-04-15 11:04:05 +0330

    Create TopPartDetailView with animation and add it to ProductDetailView

--- Online Shop App - SwiftUI Masterclass/App/ProductDetailView.swift
+++ Online Shop App - SwiftUI Masterclass/App/ProductDetailView.swift
@@ -22,6 +22,8 @@ struct ProductDetailView: View {
                 .padding(.horizontal)
             
             // DETAIL TOP PART
+            TopPartDetailView()
+                .padding(.horizontal)
             
             // DETAIL BOTTOM PART
             
---
+++ Online Shop App - SwiftUI Masterclass/View/Detail/TopPartDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  TopPartDetailView.swift
+//  Online Shop App - SwiftUI Masterclass
+//
+//  Created by Aran Fononi on 15/4/25.
+//
+
+import SwiftUI
+
+struct TopPartDetailView: View {
+    // MARK: - Property
+    @State private var isAnimating: Bool = false
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 6) {
+            // PRICE
+            VStack(alignment: .leading, spacing: 6) {
+                Text("Price")
+                    .fontWeight(.semibold)
+                
+                Text(sampleProduct.formattedPrice)
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .scaleEffect(isAnimating ? 1.35 : 0.5 , anchor: .leading)
+            } //: VStack
+            .offset(y: isAnimating ? 0 : -50)
+            
+            Spacer()
+            
+            // PHOTO
+            Image(sampleProduct.image)
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(isAnimating ? 1 : 0.5)
+                .offset(y: isAnimating ? 0 : -50 )
+            
+        } //: HStack
+        .onAppear {
+            withAnimation(.spring(duration: 0.75)) {
+                isAnimating.toggle()
+            }
+        }
+    }
+}
+
+struct TopPartDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopPartDetailView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
