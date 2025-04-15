f1ceee2ce4871377000feeb40191871a3f60783f
Aran Fononi <aaronfononi@gmail.com>
2025-04-15 10:11:20 +0330

    Create Product Detail View

---
+++ Online Shop App - SwiftUI Masterclass/App/ProductDetailView.swift
@@ -0,0 +1,43 @@
+//
+//  ProductDetailView.swift
+//  Online Shop App - SwiftUI Masterclass
+//
+//  Created by Aran Fononi on 15/4/25.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading, spacing: 5) {
+            
+            // NAVBAR
+            
+            // HEADER
+            Text(sampleProduct.name)
+            
+            // DETAIL TOP PART
+            
+            // DETAIL BOTTOM PART
+            
+            // RATINGS AND SIZES
+            
+            // DESCRIPTION
+            
+            // QUANTITY AND FAVORITES
+            
+            // ADD TO CART
+            Spacer()
+        } //: VStack
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(red: sampleProduct.red, green: sampleProduct.green, blue: sampleProduct.blue)
+        ).ignoresSafeArea(.all, edges: .all)
+        
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
